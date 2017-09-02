a048951eff2849d719ce18b1144d4e348821de0c
Sumit Kumar <sumitk.linux@gmail.com>
2017-09-02 12:13:37 -0700

    Second Commit

--- skillset.py
+++ skillset.py
@@ -1,4 +1,4 @@
-teammates_skill = {'Sumit': 'Cloud Computing', 'Vishal Kumar': 'Tandem', 'Shaishaw': 'Puppet', 'Kalyan': 'Python','Sumit': 'Linux'}
+teammates_skill = {'Sumit': 'Cloud Computing', 'Vishal Kumar': 'Tandem', 'Shaishaw': 'Puppet', 'Kalyan': 'Python','Preetham': 'Linux'}
 
 for k, v in teammates_skill.items():
     print(k, v)

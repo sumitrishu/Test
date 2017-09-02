322784ec40e8fe08285706ecc940bcda8b40aa2b
Sumit Kumar <sumitk.linux@gmail.com>
2017-09-02 12:03:40 -0700

    First Commit

---
+++ skillset.py
@@ -0,0 +1,4 @@
+teammates_skill = {'Sumit': 'Cloud Computing', 'Vishal Kumar': 'Tandem', 'Shaishaw': 'Puppet', 'Kalyan': 'Python','Sumit': 'Linux'}
+
+for k, v in teammates_skill.items():
+    print(k, v)
